e7f72c6909b006fb9f560f0a78caa04851c52086
virat-18-git <ksprddy6@gmail.com>
2024-12-26 13:01:20 +0530

    nested loops

---
+++ anything.py
@@ -0,0 +1,79 @@
+# break and continue:
+
+# for i in range(0,20):
+#     # print (i)
+#     if i>=10:
+#         print(i)
+#         continue
+    
+
+#     for i in range(0,20):
+#     # print (i)
+#      if i>=10:
+#         print(i)
+#         break
+     
+# for i in range(0,10):
+#   break
+  
+# for i in range(0,10):
+#   continue
+
+# count=0
+# for i in range(0,20):
+#   count+=1
+#   if i>=5:
+#     break
+#   print(i)
+
+
+# count=0
+# for i in range(0,20):
+#   count+=1
+#   if i>=5:
+#     continue
+#   print(i)
+
+# count=0
+# for i in range (0,10):
+#     if i>=5:
+#         print(i)
+# print(count)
+
+
+# pass:
+# num1 =25
+# if num1 % 2 == 0:
+#   pass
+
+# else:
+#   print("odd")
+
+
+# nested loops
+# curr_units =200
+# if curr_units<350:
+#   print(curr_units*10)
+# else:
+#   print(curr_units*20)
+
+# curr_units =200
+# if curr_units<350:
+#   if curr_units<=200:
+#     if curr_units <=100:
+#       print(curr_units*-5)
+#     else:
+#        print(0)
+
+#   else:
+#     print(curr_units*10)
+# else:
+#   print(curr_units*20)
+
+for i in range(1,11):
+    print("class",i)
+    for j in range(1,31):
+        print(i,j)
+
+  
+  
